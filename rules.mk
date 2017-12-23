9b008dc25c8955ee507996c0671773afd26a27be
Michael Torres <michael@notion.ai>
2017-12-22 19:35:09 -0500

    add planck files

---
+++ rules.mk
@@ -0,0 +1,6 @@
+BACKLIGHT_ENABLE = no  # Enable keyboard backlight functionality
+COMMAND_ENABLE   = no   # Disable shift combination, which conflicts with shift-parens
+MOUSEKEY_ENABLE  = yes   # Mouse keys (takes up a lot of save space)
+NKRO_ENABLE  = yes  # N-key rollover required for use as a steno board
+AUDIO_ENABLE = yes  # Audio output on port C6
+BOOTMAGIC_ENABLE = no # Bootmagic can be confusing
